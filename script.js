bc2644dfae874c367951af7b702cf4f8482a257d
Vikcarvalhos <viktorcarvalhos@gmail.com>
2023-11-20 11:36:48 -0300

    refactor: changes to the data transition

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ const dataBox = document.querySelectorAll('.data-content')
 window.addEventListener('scroll', checkBoxes);
 
 function checkBoxes() {
-    const triggerBotton = window.innerHeight = window.innerHeight;
+    const triggerBotton = window.innerHeight / 1.5;
     dataBox.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
         if (boxTop < triggerBotton) {
